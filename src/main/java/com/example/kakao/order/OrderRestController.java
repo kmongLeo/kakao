fd38aea44965f8b7841bc5698ebe76462e3f2348
kimunche <angela__id@naver.com>
2024-03-12 02:01:02 +0900

    Feat : Order API 개발 - 결제(order), 주문결과 확인(getOrdersById)

--- src/main/java/com/example/kakao/order/OrderRestController.java
+++ src/main/java/com/example/kakao/order/OrderRestController.java
@@ -2,7 +2,9 @@ package com.example.kakao.order;
 
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
+import com.example.kakao.option.OptionResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
@@ -15,14 +17,21 @@ import java.util.List;
 @RestController
 public class OrderRestController {
 
-
+    private final OrderService orderService;
 
     // (기능12) 결재
-    //@PostMapping("/orders/save")
-
+    @PostMapping("/orders/save")
+    public ResponseEntity<?> order(@AuthenticationPrincipal CustomUserDetails user){
+        OrderResponse order = orderService.order(user.getUser());
+        return ResponseEntity.ok(ApiUtils.success(order));
+    }
 
     // (기능13) 주문 결과 확인
-    //@GetMapping("/orders/{id}")
+    @GetMapping("/orders/{id}")
+    public ResponseEntity<?> getOrdersById(@PathVariable("id") int orderId, @AuthenticationPrincipal CustomUserDetails user) {
+        OrderResponse order = orderService.getOrdersById(orderId, user.getUser());
+        return ResponseEntity.ok(ApiUtils.success(order));
+    }
 
 
     // 사용 안함
--- src/main/java/com/example/kakao/order/OrderService.java
+++ src/main/java/com/example/kakao/order/OrderService.java
@@ -24,5 +24,62 @@ import java.util.Set;
 @Service
 public class OrderService {
 
+    private final OrderJPARepository orderJPARepository;
+    private final CartJPARepository cartJPARepository;
+    private final OptionJPARepository optionJPARepository;
+    private final ItemJPARepository itemJPARepository;
 
+    @Transactional
+    public OrderResponse order(User user) {
+        // 일단 장바구니 가져와
+        List<Cart> carts = cartJPARepository.findByUserId(user.getId());
+        if(carts.isEmpty()){ //없으면 던져
+            throw new Exception400("there is no carts for user : "+ user.getId());
+        }
+
+        //주문
+        Order order = Order.builder()
+                .user(user)
+                .build();
+
+        orderJPARepository.save(order);
+
+        //주문내역 저장 - carts = item
+        List<Item> items = new ArrayList<>();
+        int totalPrice = 0;
+        for(Cart cart : carts){
+            Item item = Item.builder()
+                    .option(cart.getOption())
+                    .order(order)
+                    .quantity(cart.getQuantity())
+                    .price(cart.getPrice())
+                    .build();
+
+            totalPrice += cart.getPrice();
+
+            items.add(item);
+        }
+        itemJPARepository.saveAll(items);
+
+        //장바구니 지워
+        cartJPARepository.deleteByUserId(user.getId());
+
+        return new OrderResponse(order, items, totalPrice);
+
+    }
+
+    public OrderResponse getOrdersById(int orderId, User user) {
+        Order order = orderJPARepository.findById(orderId)
+                .orElseThrow(() -> new Exception400("no orders : "+ orderId));
+
+        List<Item> itemList = itemJPARepository.findAllByOrderId(orderId);
+
+        //TODO:코드 반복됨 - 맘에 안듦
+        int totalPrice = 0;
+        for(Item item : itemList){
+            totalPrice += item.getPrice();
+        }
+
+        return new OrderResponse(order, itemList, totalPrice);
+    }
 }
